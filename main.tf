73edd22f7e80c642e924ea30725acb00650b7caa
Mr.Professor <wasim.hannure@yahoo.in>
2026-05-02 20:26:36 +0530

    update acr in the code

--- main.tf
+++ main.tf
@@ -19,3 +19,12 @@ resource "azurerm_storage_account" "professor" {
     environment = "class"
   }
 }
+
+resource "azurerm_container_registry" "acr" {
+  name                = "azureclassmay2026"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Premium"
+  admin_enabled       = false
+  
+}
